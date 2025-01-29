8fa9217491a501b71a6524ef2486dba3dfae615f
zhaoyu-z <zhangzy517@gmail.com>
2025-01-29 22:59:07 +0000

    Added test security config

---
+++ src/main/java/com/iamzzy/simpleshop/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.iamzzy.simpleshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for testing
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll() // Allow all apis
+                        .anyRequest().authenticated() // Secure other endpoints
+                );
+
+        return http.build();
+    }
+}
